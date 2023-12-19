888e241dd97893b8247f0ffb06067385d63e6531
Syafiq Termizi <ahmadsyafiq93@gmail.com>
2023-12-19 16:28:34 +0800

    Update billiard

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("HISTORY.rst") as history_file:
 requirements = [
     "pylokit==0.8.1",
     "django==4.2.8",
-    "billiard>=3.5.0.2,<3.6.0",
+    "billiard==4.2.0",
 ]
 
 test_requirements = [
